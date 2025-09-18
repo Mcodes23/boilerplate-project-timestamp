8e5f2ab76cca0cf4a1d25a7d773090651c547629
Mcodes23 <mateliboniface@gmail.com>
2025-09-18 08:50:50 +0300

    handling empty dates cases

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
+
 app.get("/api/:date", function (req, res) {
   let dateInput = req.params.date;
 
